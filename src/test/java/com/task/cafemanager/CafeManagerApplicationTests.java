37dbb32be01741e1b7f1baf9c995193cfdb3599d
hayk-arzumanyan <hayk.arzumanyan@gmail.com>
2021-02-26 18:38:06 +0400

    Project Created.

---
+++ src/test/java/com/task/cafemanager/CafeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.cafemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
